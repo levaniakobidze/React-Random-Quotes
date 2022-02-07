dc7640e44233e0446f7efc942b2fe93f9491c70b
levaniakobidze <90929516+levaniakobidze@users.noreply.github.com>
2022-02-07 18:29:16 +0400

    error

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react';
 const url = 'https://type.fit/api/quotes'
+
     
     
     
@@ -11,39 +12,53 @@ const url = 'https://type.fit/api/quotes'
         const [index,setIndex] = useState(0);
         const [quotes,setQuotes] = useState([])
         const [loading,setLoading] = useState(true);
+        const [imgIndex, setImgIndex] = useState('2')
+
         const fetchData = async () => {
             const response = await fetch(url)
             const newQuotes = await response.json();
             setQuotes(newQuotes);
             setLoading(false)
+            
            
             }
 
             useEffect(()=> {
                 fetchData();
               
-                 
+            
+         console.log('sdada')
                },[]) 
-          
+          console.log('ok')
           
             if(loading){
                 return (
                     <h1>loading...</h1>
+                    
                 )
            }
+           
+           
 
 
-           const handleClick = () => {
+           const handleClick = (e) => {
             const newIndex = Math.floor(Math.random() * (quotes.length + 1 - 0 + 1)) + 0;
-               console.log(newIndex)
-            
+              e.preventDefault()
+           
                setIndex(newIndex);
            }
+           const handleImgClick = () => {
+            const newImgIndex = Math.floor(Math.random() * (12  - 0 + 1)) + 1;
+            setImgIndex(newImgIndex)
+            console.log(newImgIndex)
+            console.log(imgIndex)
+            
+        }
+        
     
            
         
-
-         
+        
        
 
          const { text , author } = quotes[index]
@@ -51,15 +66,26 @@ const url = 'https://type.fit/api/quotes'
 
      return (
          <>
+         {}
+        
+          <div className="main">
+              <div className="main-img-cont">
+          <img src={require(`./images2/image${5}.jpg`)} alt="" />
+          </div>
            <div className="wrapper">
                <div className="text-cont">
            <h1>{text}</h1>
-           <p>{author}</p>
+           <div className="author"> 
+           
+           <p>--- {author}</p>
+           </div>
            </div>
            <div className="button-cont">
+               <button onClick={handleImgClick}>img</button>
            <button onClick={handleClick}>Next</button>
            </div>
        
+           </div>
            </div>
          
          </>
